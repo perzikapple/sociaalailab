888d8f7bea245cb93aa9f27144c4b763adeb3765
Jaivy <9019717@student.zadkine.nl>
2026-03-06 12:23:05 +0100

    kennis page column foto's max-width en height gegeven.

--- programma/kennis.php
+++ programma/kennis.php
@@ -302,7 +302,20 @@ try {
       banners[current].classList.add('active');
     }, 10000);
 </script>
+<style>
+    .flex-col {
+        display: flex;
+        flex-direction: column;
+    }
 
+    .flex-col img {
+        max-width: 550px;
+        max-height: 360px;
+        object-fit: cover;
+        display: block;
+        border-radius: 4px;
+    }
+</style>
 </body>
 </html>
 
